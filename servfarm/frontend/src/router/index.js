91fecad663da7b48477f11c5d74b0d5d0a53328f
Rui Castro <rui_castro@live.com.pt>
2021-04-22 15:04:51 +0000

    Add @Header.vue to components

--- servfarm/frontend/src/router/index.js
+++ servfarm/frontend/src/router/index.js
@@ -8,12 +8,17 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/shop',
+    name: 'Shop',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Shop.vue')
+  },
+  {
+    path: '/booking',
+    name: 'Book',
+    component: () => import('..views/Booking.vue')
   }
 ]
 
