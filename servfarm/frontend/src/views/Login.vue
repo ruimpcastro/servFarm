d4633de87fb52b6370c9a4175e64ef3382bcc6d8
Mafalda <73299421+Slendercookie@users.noreply.github.com>
2021-04-23 15:25:14 +0100

    Created login pages
    Por continuar

--- servfarm/frontend/src/views/Login.vue
+++ servfarm/frontend/src/views/Login.vue
@@ -1,15 +1,60 @@
 <template>
-  <div class="page-structure">
-      
-  </div>
+    <div id="login">
+      <div>
+        <img
+        id = "imagem1"
+  src="https://www.gettyimages.pt/gi-resources/images/500px/983794168.jpg"
+  alt="Passarim"
+  class="center"
+  >
+        <h1>Login</h1>
+        <input type="text" name="username" v-model="input.username" placeholder="Username" />
+        <input type="password" name="password" v-model="input.password" placeholder="Password" />
+        <button type="button" v-on:click="login()">Login</button>
+      </div>
+    </div>
 </template>
 
 <script>
-export default {
-
-}
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                input: {
+                    username: "",
+                    password: ""
+                }
+            }
+        },
+        methods: {
+            login() {
+                if(this.input.username != "" && this.input.password != "") {
+                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
+                        this.$emit("authenticated", true);
+                        this.$router.replace({ name: "secure" });
+                    } else {
+                        console.log("The username and / or password is incorrect");
+                    }
+                } else {
+                    console.log("A username and password must be present");
+                }
+            }
+        }
+    }
 </script>
 
-<style>
-
+<style scoped>
+    #login {
+        width: 500px;
+        border: 1px solid #CCCCCC;
+        background-color: #FFFFFF;
+        margin: auto;
+        margin-top: 200px;
+        padding: 20px;
+    }
+    #imagem1 {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
 </style>
