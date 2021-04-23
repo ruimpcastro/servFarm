9a3823d6efdc821b57311b4fb7f93d83cb4cbea1
Rui Castro <rui_castro@live.com.pt>
2021-04-23 15:20:16 +0000

    Added style.css
    Created HTML for @views/User and @views/Register

--- servfarm/frontend/src/views/Register.vue
+++ servfarm/frontend/src/views/Register.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="page-structure">
-      
+      <form action="">
+        <label for="fname">First name:</label>
+        <input type="text" placeholder="First name">
+
+        <label for="lname">Last name:</label>
+        <input type="text" placeholder="Last name">
+
+        <label for="email">E-mail:</label>
+        <input type="text" placeholder="E-mail">
+
+        <label for="pword">Password:</label>
+        <input type="text" placeholder="Password">
+
+        <label for="cellphone">Cellphone number:</label>
+        <input type="text" placeholder="Number">
+
+        <button type="submit">Register</button>
+      </form>
+
   </div>
 </template>
 
--- servfarm/frontend/src/views/User.vue
+++ servfarm/frontend/src/views/User.vue
@@ -1,6 +1,44 @@
 <template>
   <div class="page-structure">
-      
+      <div id="user">
+        <!-- SERVIÇOS AGENDADOS -->
+        <div class="scheduled">
+          <h2>Scheduled services</h2>
+          <ul>
+            <li class="sheduled-services"></li>
+          </ul>
+        </div>
+        <!-- SERVIÇOS PRESTADOS -->
+        <div class="provided">
+          <h2>Provided services</h2>
+          <ul>
+            <li class="provided-services"></li>
+          </ul>
+        </div>
+        <!-- SERVIÇOS RELACIONADOS -> VAI LEVAR HARDCODED -->
+        <div id="related">
+          <h2>Related services</h2>
+          <ul>
+            <li id="related-services"></li>
+          </ul>
+        </div>
+      </div>
+
+      <div id="admin">
+        <div class="scheduled">
+          <h2>Scheduled services</h2>
+          <ul>
+            <li class="sheduled-services"></li>
+          </ul>
+        </div>
+        <!-- SERVIÇOS PRESTADOS -->
+        <div class="provided">
+          <h2>Provided services</h2>
+          <ul>
+            <li class="provided-services"></li>
+          </ul>
+        </div>
+      </div>
   </div>
 </template>
 
