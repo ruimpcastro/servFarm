63f8b45e75e06bb7c3d30a50414456c2312c2b91
Rui Castro <rui_castro@live.com.pt>
2021-04-23 22:07:57 +0000

    Added global style to app

--- servfarm/frontend/src/App.vue
+++ servfarm/frontend/src/App.vue
@@ -18,12 +18,4 @@ export default({
 })
 </script>
 
-<style>
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-
-}
-</style>
+<style src="./assets/style.css"></style>
