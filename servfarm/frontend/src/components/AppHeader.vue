f458ba66a34cc9f195f1bb6460c87b00dec35ba7
Rui Castro <rui_castro@live.com.pt>
2021-04-23 23:39:29 +0000

    Updated AppHeader style in style.css

--- servfarm/frontend/src/components/AppHeader.vue
+++ servfarm/frontend/src/components/AppHeader.vue
@@ -1,15 +1,14 @@
 <template>
 <div id="header">
-  <div id="title">
-  <h1>Farmácia Online São Miguel</h1>
-  </div>
+  <h1 id="title">Farmácia Online São Miguel</h1>
+
   <div id="head-nav">
     <nav id="navbar">
-      <router-link :to="{ name: 'Home' }">Home </router-link>
-      <router-link :to="{ name: 'Services' }">Services </router-link>
-      <router-link :to="{ name: 'Notifications'}">Notifications </router-link>
-      <router-link :to="{ name: 'Recommendations' }">Recommendation </router-link>
-      <router-link :to="{ name: 'Login' }">Sign up</router-link>
+      <router-link class="navbar-link" :to="{ name: 'Home' }">Home </router-link>
+      <router-link class="navbar-link" :to="{ name: 'Services' }">Services </router-link>
+      <router-link class="navbar-link" :to="{ name: 'Notifications'}">Notifications </router-link>
+      <router-link class="navbar-link" :to="{ name: 'Recommendations' }">Recommendation </router-link>
+      <router-link class="navbar-link" :to="{ name: 'Login' }">Sign up</router-link>
       <!-- Missing User, Admin and Services -->
     </nav>
   </div>
@@ -24,34 +23,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#title {
-  text-align: center;
-  background-color: turquoise;
-  background: top;
 
-}
-#head-nav {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: rgb(6, 177, 168);
-  position: fixed;
-  top: 60px;
-  width: 100%;
-  border-radius: 25px;
-}
-#head-nav {
-  float: center;
-}
-#navbar {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-#navbar:hover {
-  background-color: rgb(2, 196, 170); /* Change the link color to this other color on hover */
-}
 </style>
