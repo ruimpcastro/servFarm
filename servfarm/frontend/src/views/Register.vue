47b18c8ada1980199b71e65edb3a1b3a151b78f2
Rui Castro <rui_castro@live.com.pt>
2021-04-24 18:17:47 +0000

    Added style to @Login.vue

--- servfarm/frontend/src/views/Register.vue
+++ servfarm/frontend/src/views/Register.vue
@@ -2,19 +2,19 @@
   <div class="page-structure">
       <form class="form-style" action="">
         <label for="fname">First name:</label>
-        <input type="text" placeholder="First name">
+        <input class="text-input-register" type="text" placeholder="First name">
 
         <label for="lname">Last name:</label>
-        <input type="text" placeholder="Last name">
+        <input class="text-input-register" type="text" placeholder="Last name">
 
         <label for="email">E-mail:</label>
-        <input type="text" placeholder="E-mail">
+        <input class="text-input-register" type="text" placeholder="E-mail">
 
         <label for="cellphone">Cellphone number:</label>
-        <input type="text" placeholder="Number">
+        <input class="text-input-register" type="text" placeholder="Number">
         
         <label for="pword">Password:</label>
-        <input type="text" placeholder="Password">
+        <input class="text-input-register" type="text" placeholder="Password">
 
         
 
