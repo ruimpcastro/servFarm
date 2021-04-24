1d5f485012f8e93c1d7a9505a66691697d416dc1
Rui Castro <rui_castro@live.com.pt>
2021-04-24 15:18:24 +0000

    Updated @Home.vue style

--- servfarm/frontend/src/views/Home.vue
+++ servfarm/frontend/src/views/Home.vue
@@ -1,51 +1,56 @@
 <template>
   <div class="page-structure">
-    <h1 id="intro">Hi, We're Farmácia Online São Miguel</h1>
-    <div class="product">
-      <ul>
-        <li class="product-list">
-          <img src="https://scontent.fpdl2-1.fna.fbcdn.net/v/t1.6435-9/78422932_10214557003308190_2702566720278626304_n.jpg?_nc_cat=100&_nc_map=test-rt&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=wAY2nFXeQj0AX_NbMmF&_nc_oc=AQn76OjjsZU2nglHIMofcnVugo2AcrLl03K-PQyeCteji8VevGw_Vx3wVGvDQTejB6sWkV21xj6T7QJzcDD9elba&_nc_ht=scontent.fpdl2-1.fna&oh=9e5ca41e3e0b2a10605199a0326f3077&oe=60A9DC76" alt="">
-          <p>Im a product</p>
-          <div>
-          <span class="euro">85</span>,<span class="cent">00</span>€
-          </div>
-        </li>
-      </ul>
-    </div>
-    
-    <div id="description">
-      <h3 id="description-title">A Pharmacy for everyone</h3>
-      <h4 id="description-subtitle">Responsible Healing</h4>
-      <p class="description-text">
-        We care about the well-being of 
-        each customer who walks through our doors. 
-        That’s s why we’ve dedicated so much time
-        to ensuring we stock the widest range of medical products,
-        offer the highest quality health services,
-        and retain the best staff possible.
-        </p>
+    <div id="home-style">
+      <div id="home-size">
+      <h1 id="intro">Hi, We're Farmácia Online São Miguel</h1>
+      <div class="product">
+        <img id="intro-img" src="https://scontent.fpdl2-1.fna.fbcdn.net/v/t1.6435-9/78422932_10214557003308190_2702566720278626304_n.jpg?_nc_cat=100&_nc_map=test-rt&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=wAY2nFXeQj0AX_NbMmF&_nc_oc=AQn76OjjsZU2nglHIMofcnVugo2AcrLl03K-PQyeCteji8VevGw_Vx3wVGvDQTejB6sWkV21xj6T7QJzcDD9elba&_nc_ht=scontent.fpdl2-1.fna&oh=9e5ca41e3e0b2a10605199a0326f3077&oe=60A9DC76" alt="">
+        <ul class="align-li">
+          <img src="" alt="Product img" srcset="">
+          <li class="product-list">
+            <p>Im a product</p>
+            <div>
+            <span class="euro">85</span>,<span class="cent">00</span>€
+            </div>
+          </li>
+        </ul>
+      </div>
+      
+      <div id="description">
+        <h3 id="description-title">A Pharmacy for everyone</h3>
+        <h4 id="description-subtitle">Responsible Healing</h4>
         <p class="description-text">
-        As a middleman between your doctor’s prescriptions and
-        your own well being, we strive to provide personalized
-        and professional attention, as well as medication
-        management protocols that inspire customers to seek out
-        the medications and advice they need in order to get better.
-        Trust our Pharmacy to provide you with the care you deserve.
-        </p>
-    </div>
-    <div id="about">
-      <h3 id="about-title">Opening hours</h3>
-      <h4 id="about-subtitle">Come visit</h4>
-      <ul id="about-text">
-        <li>Mon-Fri: 9am - 6pm</li>
-        <li>Sat: 10am - 2am</li>
-        <li>Sun: Closed</li>
-      </ul>
-    </div>
-    <!-- Eventos e calendário para utilizadores autenticados -->
-    <div id="btn-agenda">
-      <button class="agenda">Check agenda</button>
-      <button class="agenda">Mark an appointment</button>
+          We care about the well-being of 
+          each customer who walks through our doors. 
+          That’s s why we’ve dedicated so much time
+          to ensuring we stock the widest range of medical products,
+          offer the highest quality health services,
+          and retain the best staff possible.
+          </p>
+          <p class="description-text">
+          As a middleman between your doctor’s prescriptions and
+          your own well being, we strive to provide personalized
+          and professional attention, as well as medication
+          management protocols that inspire customers to seek out
+          the medications and advice they need in order to get better.
+          Trust our Pharmacy to provide you with the care you deserve.
+          </p>
+      </div>
+      <div id="about">
+        <h3 id="about-title">Opening hours</h3>
+        <h4 id="about-subtitle">Come visit</h4>
+        <ul id="about-text">
+          <li>Mon-Fri: 9am - 6pm</li>
+          <li>Sat: 10am - 2am</li>
+          <li>Sun: Closed</li>
+        </ul>
+      </div>
+      <!-- Eventos e calendário para utilizadores autenticados -->
+      <div id="btn-agenda">
+        <button class="agenda">Check agenda</button>
+        <button class="agenda">Mark an appointment</button>
+        </div>
+      </div>
     </div>
   </div>
 
